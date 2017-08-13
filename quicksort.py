f7ebea520e2017cd4aeec4fb095844c29c25a815
CHBaker <charlesprobaker@mail.com>
2017-08-13 16:32:41 -0600

    CHORE: add print statements
    ADD print statements for debugging
    
    - shows algo. process
    - step by step
    - formatted clearly

--- quicksort.py
+++ quicksort.py
@@ -8,7 +8,7 @@ def compare(pivot, array, start):
         while pivot >= element:
             start += 1
             element = array[start]
-            print "loop ---- index of new element count : ", start
+            print "[][][] loop ---- index of new element count : ", start, " [][][]"
         return start
 
 # helper function moves items within list for in-place sort        
@@ -17,50 +17,60 @@ def move(start, array, pivotIndex, end):
         array.insert(end, array[start])
         # delete copy of first element
         array.pop(start)
-        print "first move : ", array
+        print "! first move : "
+        print ""
+        print array
+        print ""
         
         # move item left of pivot to last element to the front of the list
         array.insert(start, array[pivotIndex - 2])
         # remove copy of said element
         array.pop(pivotIndex - 1)
-        print "second move : ", array
+        print "! second move : "
+        print ""
+        print array
+        print ""
         
         return array
         
 def quicksort(array, start, end):
     i = 1
-    point = start + 1
+    point = start
+    print "* initial point: ", point
     pivot = array[(len(array[start: end]) - point)]
+    print "* initial pivot: ", pivot
     pivotIndex = (len(array[start: end]) - point)
+    print "* intial pivot index", pivotIndex
     
     while i <= ((len(array[start: end]) - 1)):
         startval = array[start]
-        print "start :", start, "pivot :", pivot
+        print "[][][] start : ", start, " }{ pivot : ", pivot, " [][][]"
         
         if pivot < start:
             pivotIndex = len(array[start: end]) - point
             array = move(start, array, pivotIndex, end)
             # update pivot location by changing point
             point += 1
-            print "point : ", point
-            print "index: ", pivotIndex
+            print "[][][] point : ", point, " [][][]"
+            print "[][][] index: ", pivotIndex, " [][][]"
             i += 1
-            print "pivot", pivot
+            print "[][][] pivot: ", pivot, " [][][]"
             startval = array[start]
             
         elif pivot >= start:
-            print "elif move"
+            print "[][][] elif move [][][]"
             pivotIndex = len(array[start: end]) - point
-            print "---arrays of elif----"
+            print "[][][] --- arrays of elif ---- [][][]"
             array = move((compare(pivot, array, start)), array, pivotIndex, end)
             # update pivot location by changing point
             point += 1
-            print "point : ", point
+            print "[][][] point : ", point, " [][][]"
             startval = array[start]
         
             i += 1
             
-    print "completed list  ", array
+    print "[][][] completed list [][][]"
+    print array
             
             
         
